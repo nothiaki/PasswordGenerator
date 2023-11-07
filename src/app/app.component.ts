3975b9e8840d465262fb9e7139525770ce08134f
NotHiaki <celsohenrique367@gmail.com>
2023-11-07 19:55:42 -0300

    feat: fetch in password generator api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { password } from './interfaces/password';
+import { GeneratePasswordService } from './services/generate-password.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   length: number = 12;
+  password: string = '';
+
+  constructor (private generatePasswordService: GeneratePasswordService) {}
+
+  generatePass(): void {
+    this.generatePasswordService.getPassword().subscribe(data => this.password = data.random_password);
+  }
 }

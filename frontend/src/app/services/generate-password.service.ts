e7dc56e3e33fb6133ee3aa4548f64378a68fe454
NotHiaki <celsohenrique367@gmail.com>
2023-12-23 18:21:30 -0300

    chore: generate-password service

--- frontend/src/app/services/generate-password.service.ts
+++ frontend/src/app/services/generate-password.service.ts
@@ -8,14 +8,11 @@ import { password } from '../interfaces/password';
 })
 export class GeneratePasswordService {
 
-  url: string = 'https://api.api-ninjas.com/v1/passwordgenerator?length=';
-  headers: HttpHeaders = new HttpHeaders({
-    'X-Api-Key': 'EST4yYPppJBoVwIiCJvOm5RthG5IPH0P1mNuoY8d'
-  });
+  url: string = 'https://random-password-api.vercel.app/';
 
   constructor(private http: HttpClient) { }
 
   getPassword(length: number): Observable<password> {
-    return this.http.get<password>(this.url + length, { headers: this.headers });
+    return this.http.get<password>(this.url + length);
   }
 }
